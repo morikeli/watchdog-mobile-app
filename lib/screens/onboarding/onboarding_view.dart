7d88f9f5c0191601e8dd681773e657875eb5f17a
brocode <kelimbth@gmail.com>
2024-04-02 11:49:59 +0300

    onboarding scrreen items

---
+++ lib/screens/onboarding/onboarding_view.dart
@@ -0,0 +1,22 @@
+import 'package:watchdog/screens/onboarding/onboarding_info.dart';
+
+
+class OnboardingItems {
+  List<OnboardingInfo> items = [
+    OnboardingInfo(
+      title: "Report road accidents and crimes", 
+      description: "Report crimes or accidents using your mobile device or computer.", 
+      image: "assets/onboarding/animation1.gif",
+    ),
+    OnboardingInfo(
+      title: "Sheria mkononi", 
+      description: "Help law enforcement agents to maintain law and order by reporting crimes or reporting wanted suspects anonymously.", 
+      image: "assets/onboarding/animation2.gif",
+    ),
+    OnboardingInfo(
+      title: "Crimes and accidents locations", 
+      description: "Get live feed for reported crimes and road accidents in your current location.", 
+      image: "assets/onboarding/animation3.gif",
+    ),
+  ];
+}
