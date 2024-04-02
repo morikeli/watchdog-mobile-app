3c40d0f1160dda916274a0dd5a8799a184c450c4
brocode <kelimbth@gmail.com>
2024-04-02 12:06:48 +0300

    replaced LoginPage() with Homepage()

--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-import 'package:watchdog/pages/login.dart';
+import 'package:watchdog/pages/homepage.dart';
 import 'package:watchdog/screens/onboarding/onboarding_view.dart';
 
 
@@ -107,7 +107,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         onPressed: () {
           Navigator.pushReplacement(context, 
             MaterialPageRoute(
-              builder: ((context) => LoginPage())
+              builder: ((context) => Homepage())
             )
           );
         }, 
