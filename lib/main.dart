6069da19c23f61c1416b52809b300b7aafd4d45d
brocode <kelimbth@gmail.com>
2024-04-04 17:45:09 +0300

    imported new dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:watchdog/pages/map.dart';
 import 'package:watchdog/pages/suspects.dart';
 import 'package:watchdog/screens/onboarding/onboarding.dart';
+import 'package:watchdog/screens/onboarding/splash_screen.dart';
 import 'pages/homepage.dart';
 
 
