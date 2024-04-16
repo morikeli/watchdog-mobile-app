ba2059ccae9e7f7059292c761dfcaf70fb6cda6a
brocode <kelimbth@gmail.com>
2024-04-16 21:01:35 +0300

    wrapped Column() with SingleChildScrollView()

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -39,101 +39,103 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
       body:  Padding(
         padding: const EdgeInsets.all(30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: CircleAvatar(
-                backgroundColor: Colors.grey.shade300,
-                backgroundImage: const AssetImage('assets/profile-pic.jpg'),
-                radius: 100.0,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey.shade300,
+                  backgroundImage: const AssetImage('assets/profile-pic.jpg'),
+                  radius: 100.0,
+                ),
               ),
-            ),
-            const Divider(height: 60.0),
-            Row(
-              children: [
-                Text('Username', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 70.0),
-                const Text('Testuser',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            const SizedBox(height: 10.0),
-            Row(
-              children: [
-                Text('Gender', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 96.0),
-                const Text('Male',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            const SizedBox(height:10.0),
-            Row(
-              children: [
-                Text('Date of birth', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 54.0),
-                const Text('January 1, 2000',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            const SizedBox(height:10.0),
-            Row(
-              children: [
-                Text('Age', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 122.0),
-                const Text('23',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            const SizedBox(height:10.0),
-            Row(
-              children: [
-                Text('Mobile no.', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 70.0),
-                const Text('+254112345678',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            const SizedBox(height:10.0),
-            Row(
-              children: [
-                Text('County', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 100.0),
-                const Text('Nairobi',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-            const SizedBox(height:10.0),
-            Row(
-              children: [
-                Text('Sub county', style: TextStyle(
-                  fontSize: 18.0, 
-                  fontWeight: FontWeight.w600,
-                  color: Colors.grey.shade500,
-                )),
-                const SizedBox(width: 68.0),
-                const Text('Embakasi',  style: TextStyle(fontSize: 18.0)),
-              ],
-            ),
-          ],
+              const Divider(height: 60.0),
+              Row(
+                children: [
+                  Text('Username', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 70.0),
+                  const Text('Testuser',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Row(
+                children: [
+                  Text('Gender', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 96.0),
+                  const Text('Male',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              const SizedBox(height:10.0),
+              Row(
+                children: [
+                  Text('Date of birth', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 54.0),
+                  const Text('January 1, 2000',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              const SizedBox(height:10.0),
+              Row(
+                children: [
+                  Text('Age', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 122.0),
+                  const Text('23',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              const SizedBox(height:10.0),
+              Row(
+                children: [
+                  Text('Mobile no.', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 70.0),
+                  const Text('+254112345678',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              const SizedBox(height:10.0),
+              Row(
+                children: [
+                  Text('County', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 100.0),
+                  const Text('Nairobi',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+              const SizedBox(height:10.0),
+              Row(
+                children: [
+                  Text('Sub county', style: TextStyle(
+                    fontSize: 18.0, 
+                    fontWeight: FontWeight.w600,
+                    color: Colors.grey.shade500,
+                  )),
+                  const SizedBox(width: 68.0),
+                  const Text('Embakasi',  style: TextStyle(fontSize: 18.0)),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
