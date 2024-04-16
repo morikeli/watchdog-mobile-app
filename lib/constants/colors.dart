76ffb487622cc883e0dc6099915d24b24cd553d0
brocode <kelimbth@gmail.com>
2024-04-16 15:01:15 +0300

    deleted extra whitespace

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 
 var kScaffoldBackgroundColor = const Color(0xFFFFFFFF).withOpacity(0.9);
-const  kPrimaryColor = Color(0xFF0D47A1);
-const  kAppBarColor = Color(0xFF0D47A1);
+const kPrimaryColor = Color(0xFF0D47A1);
+const kAppBarColor = Color(0xFF0D47A1);
 const kIconColor = Color(0xFFFFFFFF);
 const kTextColor = Color(0xFFFFFFFF);
