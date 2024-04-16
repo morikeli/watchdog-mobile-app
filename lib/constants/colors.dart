be4455e93986b4b8eb958939031cec9fca0f438d
brocode <kelimbth@gmail.com>
2024-04-16 21:17:27 +0300

    updated kScaffoldBackgroundColor

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 
-var kScaffoldBackgroundColor = const Color(0xFFFFFFFF).withOpacity(0.9);
+const kScaffoldBackgroundColor = Color(0xFFBDBDBD);
 const kPrimaryColor = Color(0xFF0D47A1);
 const kAppBarColor = Color(0xFF0D47A1);
 const kIconColor = Color(0xFFFFFFFF);
