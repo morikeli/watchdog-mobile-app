89c8e30b893500750e234756e8dcdb444305bc73
brocode <kelimbth@gmail.com>
2024-04-18 15:19:37 +0300

    updated onboarding screen items

--- lib/screens/onboarding/onboarding_view.dart
+++ lib/screens/onboarding/onboarding_view.dart
@@ -4,19 +4,29 @@ import 'package:watchdog/screens/onboarding/onboarding_info.dart';
 class OnboardingItems {
   List<OnboardingInfo> items = [
     OnboardingInfo(
-      title: "Report road accidents and crimes", 
+      title: "Piga ripoti", 
       description: "Report crimes or accidents using your mobile device or computer.", 
-      image: "assets/onboarding/animation1.gif",
+      image: "assets/onboarding/report-crime.gif",
     ),
     OnboardingInfo(
       title: "Sheria mkononi", 
-      description: "Help law enforcement agents to maintain law and order by reporting crimes or reporting wanted suspects anonymously.", 
-      image: "assets/onboarding/animation2.gif",
+      description: "Help law enforcement agents to maintain law and order by reporting crimes or reporting wanted suspects.", 
+      image: "assets/onboarding/law.gif",
     ),
     OnboardingInfo(
-      title: "Crimes and accidents locations", 
-      description: "Get live feed for reported crimes and road accidents in your current location.", 
-      image: "assets/onboarding/animation3.gif",
+      title: "Okoa maisha", 
+      description: "Help road accident victims get faster medical attention by reporting fatal road accidents in your current location.", 
+      image: "assets/onboarding/medical-attention.gif",
+    ),
+    OnboardingInfo(
+      title: "Uhondo kamili", 
+      description: "Get updates about the latest reported incidents on your news feed.", 
+      image: "assets/onboarding/news-feed.gif",
+    ),
+    OnboardingInfo(
+      title: "Kaa chonjo kwa streets",
+      description: "Plan your routes or visitation in advance by viewing incidents reported in your current location on a map.",
+      image: "assets/onboarding/location.gif",
     ),
   ];
 }
