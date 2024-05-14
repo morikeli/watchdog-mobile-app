a04733943e6f609b45d5e9f3f7e2253018761e67
brocode <kelimbth@gmail.com>
2024-05-14 13:51:18 +0300

    updated indentation

--- lib/components/login_form.dart
+++ lib/components/login_form.dart
@@ -16,82 +16,82 @@ class LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Text(
-                'Welcome back', 
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35.0,
-                  color: Colors.black54,
-                ),
-                textAlign: TextAlign.center,
+      padding: const EdgeInsets.all(16.0),
+      child: Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text(
+              'Welcome back', 
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 35.0,
+                color: Colors.black54,
               ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: username,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.person_outline),
-                  prefixIconColor: Colors.grey,
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  hintStyle: TextStyle(color: Colors.grey.shade500),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(28),
-                    borderSide: const BorderSide(color: Colors.grey),
-                    gapPadding: 10,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(28),
-                    borderSide: const BorderSide(color: Colors.grey),
-                    gapPadding: 10,
-                  ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: username,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.person_outline),
+                prefixIconColor: Colors.grey,
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                hintStyle: TextStyle(color: Colors.grey.shade500),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  borderSide: const BorderSide(color: Colors.grey),
+                  gapPadding: 10,
                 ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter username';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: password,
-                decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.lock_outline),
-                  prefixIconColor: Colors.grey,
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  hintStyle: TextStyle(color: Colors.grey.shade500),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(28),
-                    borderSide: const BorderSide(color: Colors.grey),
-                    gapPadding: 10,
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(28),
-                    borderSide: const BorderSide(color: Colors.grey),
-                    gapPadding: 10,
-                  ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  borderSide: const BorderSide(color: Colors.grey),
+                  gapPadding: 10,
                 ),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter password';
-                  }
-                  return null;
-                },
               ),
-            
-            ],
-          ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter username';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: password,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.lock_outline),
+                prefixIconColor: Colors.grey,
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                hintStyle: TextStyle(color: Colors.grey.shade500),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 22, vertical: 10),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  borderSide: const BorderSide(color: Colors.grey),
+                  gapPadding: 10,
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(28),
+                  borderSide: const BorderSide(color: Colors.grey),
+                  gapPadding: 10,
+                ),
+              ),
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter password';
+                }
+                return null;
+              },
+            ),
+          
+          ],
         ),
-      );
+      ),
+    );
   }
 }
